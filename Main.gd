b9e113a90a21f8815c1e60c6200d49a92afc1be5
Jamie Wood <jamie.wood70@ntlworld.com>
2021-06-11 23:16:57 +0100

    Wire up ships to timeline manager

--- Main.gd
+++ Main.gd
@@ -1,10 +1,5 @@
 extends Node
 
 
-const Ship = preload("res://Ships/Ship.tscn")
-
-
 func _ready():
-    # Test code
-    var ship := Ship.instance()
-    add_child(ship)
+    pass
--- TimelineManager.gd
+++ TimelineManager.gd
@@ -1,8 +1,12 @@
 extends Node
 
 
+const Ship = preload("res://Ships/Ship.tscn")
+
+
 var live_timeline: int = 0
 var timelines: Array = []
+var ships: Array = []
 var frame_num: int = 0
 
 
@@ -10,19 +14,26 @@ func _ready() -> void:
     # TODO create new timeline if needed (always do this for now)
     timelines.append(Timeline.new())
     timelines.append(Timeline.new())
-    # TODO spawn in a ship for each timeline
+
+    # Spawn in a ship for each timeline
+    ships = []
+    for tl in timelines:
+        var ship := Ship.instance()
+        ships.append(ship)
+        add_child(ship)
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
     # Get inputs and append to 'live' timeline if changed
     timelines[live_timeline].set_state(frame_num, InputManager.get_state())
 
     print("---")
-    for tl in timelines:
-        # Get current input state for this ship
-        var state = tl.get_state(frame_num)
-        print(state.is_pressed(InputType.FIRE))
+    for i in range(len(timelines)):
+        var tl: Timeline = timelines[i]
+        var ship: Node2D = ships[i]
 
-        # TODO poke current input state into this ship and run tick
+        # Get current input state for this ship and run tick
+        var state = tl.get_state(frame_num)
+        ship.run_step(state, delta)
 
     frame_num += 1
