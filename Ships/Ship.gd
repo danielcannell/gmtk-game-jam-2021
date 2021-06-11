d7d93968601155cd298ca50bc4481c4eb606e24d
Daniel Cannell <danielcannell@googlemail.com>
2021-06-11 23:00:28 +0100

    Make the ship move based on the inputs

--- Ships/Ship.gd
+++ Ships/Ship.gd
@@ -1,15 +1,28 @@
 extends Area2D
 
 
+const HORIZONTAL_SPEED = 100
+const FORWARD_SPEED = 100
+const REVERSE_SPEED = 100
+
+
 func _ready():
     pass
 
 
-func run_step(input, delta: float) -> void:
+func run_step(inputs: InputState, delta: float) -> void:
     var velocity := Vector2(0, 10)
 
-    var screen_size = get_viewport_rect().size
+    if inputs.is_pressed(InputType.UP):
+        velocity.y -= FORWARD_SPEED
+    if inputs.is_pressed(InputType.DOWN):
+        velocity.y += REVERSE_SPEED
+    if inputs.is_pressed(InputType.LEFT):
+        velocity.x -= HORIZONTAL_SPEED
+    if inputs.is_pressed(InputType.RIGHT):
+        velocity.x += HORIZONTAL_SPEED
 
+    var screen_size = get_viewport_rect().size
 
     position += velocity * delta
     position.x = clamp(position.x, 0, screen_size.x)
