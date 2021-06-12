1b55f15bcec3f7cc028429d568edceb89937da3c
Jamie Wood <jamie.wood70@ntlworld.com>
2021-06-12 15:54:37 +0100

    Add bullet damage

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -6,6 +6,7 @@ const MAX_HP = 100.0
 
 onready var health_bar: Node2D = $HealthBar
 onready var area: Area2D = $Area2D
+onready var bullet_manager: BulletManager = $"../../../BulletManager"
 
 var health := MAX_HP
 
@@ -23,8 +24,16 @@ func _physics_process(delta: float) -> void:
         queue_free()
 
     health -= 5 * delta
-    health_bar.set_fraction(health / MAX_HP)
+    set_health(health)
 
 
 func _on_area_shape_entered(area_id: int, _area: Area2D, area_shape: int, _local_shape: int) -> void:
     print(area_id, " - ", area_shape)
+    var bullet = bullet_manager.get_bullet(area_shape)
+    bullet.dead = true
+    set_health(health - bullet.damage)
+
+
+func set_health(new_health: float) -> void:
+    health = new_health
+    health_bar.set_fraction(health / MAX_HP)
