1a4dce094287e61c45ddc71acf3d39dadebeee23
Daniel Cannell <danielcannell@googlemail.com>
2021-06-12 10:51:27 +0100

    Spawn infinite enemies

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -4,5 +4,13 @@ extends PathFollow2D
 const SPEED = 200
 
 
-func run_step(delta: float) -> void:
+func _ready():
+    # For now all enemies make one pass along their track
+    loop = false
+
+
+func _physics_process(delta: float) -> void:
     offset += SPEED * delta
+
+    if unit_offset > 0.99:
+        queue_free()
--- Enemies/EnemyManager.gd
+++ Enemies/EnemyManager.gd
@@ -6,24 +6,25 @@ const Enemy = preload("res://Enemies/Enemy.tscn")
 
 onready var path = $Path2D
 
-var enemy = null
+
+var enemies = []
+var frame_num = 0
 
 
-func _ready():
-    create_enemies()
-
-
-func create_enemies() -> void:
-    # Test code for now
-    enemy = Enemy.instance()
-    path.add_child(enemy)
-
-
-
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
     # Test code for now:
     if Input.is_action_just_pressed("timeline"):
-        enemy.queue_free()
-        create_enemies()
+        for enemy in enemies:
+            if is_instance_valid(enemy):
+                enemy.queue_free()
 
-    enemy.run_step(delta)
+        enemies = []
+
+        frame_num = 0
+
+    frame_num += 1
+
+    if frame_num % 120 == 0:
+        var enemy = Enemy.instance()
+        path.add_child(enemy)
+        enemies.append(enemy)
