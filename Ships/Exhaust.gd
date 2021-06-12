5fa554eca3f9b87e4f5dd1bd4cbc670b59854af8
Robert McGregor <rmcgregor1990@gmail.com>
2021-06-12 11:23:09 +0100

    add missing resources

---
+++ Ships/Exhaust.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+onready var cone = $Cone
+onready var scaler = $Scaler
+
+# Declare member variables here. Examples:
+var height = 80;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+func set_thrust(t):
+    scaler.scale.y = t
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
