3b736dcd6f9702f42047c8ad88cbae2874ddb3bd
Daniel Cannell <danielcannell@googlemail.com>
2021-06-12 22:27:27 +0100

    Make key detection a bit more robust

--- TimelineSelector.gd
+++ TimelineSelector.gd
@@ -18,19 +18,20 @@ func _ready():
 
 
 func _input(evt):
-    if evt is InputEventKey and evt.scancode == KEY_N:
-        timelines.append(Timeline.new())
-        Globals.live_timeline = len(timelines) - 1
-        get_tree().change_scene("res://Main.tscn")
-    if evt is InputEventKey and evt.scancode == KEY_0:
-        if len(timelines) > 0:
-            Globals.live_timeline = 0
+    if evt is InputEventKey and evt.pressed and !evt.echo:
+        if evt.scancode == KEY_N:
+            timelines.append(Timeline.new())
+            Globals.live_timeline = len(timelines) - 1
             get_tree().change_scene("res://Main.tscn")
-    if evt is InputEventKey and evt.scancode == KEY_1:
-        if len(timelines) > 1:
-            Globals.live_timeline = 1
-            get_tree().change_scene("res://Main.tscn")
-    if evt is InputEventKey and evt.scancode == KEY_2:
-        if len(timelines) > 2:
-            Globals.live_timeline = 2
-            get_tree().change_scene("res://Main.tscn")
+        if evt.scancode == KEY_0:
+            if len(timelines) > 0:
+                Globals.live_timeline = 0
+                get_tree().change_scene("res://Main.tscn")
+        if evt.scancode == KEY_1:
+            if len(timelines) > 1:
+                Globals.live_timeline = 1
+                get_tree().change_scene("res://Main.tscn")
+        if evt.scancode == KEY_2:
+            if len(timelines) > 2:
+                Globals.live_timeline = 2
+                get_tree().change_scene("res://Main.tscn")
