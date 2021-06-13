828fa666132de32c2a650fcd0e61960e3932eca0
Robert McGregor <rmcgregor1990@gmail.com>
2021-06-13 12:14:40 +0100

    add sheild ship type

--- Ships/Ship.gd
+++ Ships/Ship.gd
@@ -16,12 +16,45 @@ onready var bullet_manager: BulletManager = $"../../BulletManager"
 onready var tween = $Tween
 
 
+enum ShipType {
+    BULLET,
+    SHIELD
+}
+
+enum Frames {
+    FLAT = 0,
+    LEFT = 1,
+    RIGHT = 2,
+}
+
+const CONFIG = {
+    ShipType.BULLET: {
+       "frames": {
+           Frames.FLAT: 0,
+           Frames.LEFT: 1,
+           Frames.RIGHT: 2
+       },
+       "exhaust_turn_ofs": Vector2(3, 0),
+       "exhaust_turn_y_ofs": -5,
+    },
+    ShipType.SHIELD: {
+        "frames": {
+            Frames.FLAT: 5,
+            Frames.LEFT: 3,
+            Frames.RIGHT: 4
+        },
+        "exhaust_turn_ofs": Vector2(3, 0),
+        "exhaust_turn_y_ofs": -5,
+    }
+}
+
+
 var health := MAX_HP
 var fire_cooldown := 0
 
-var left_exhaust_init_pos = Vector2();
-var right_exhaust_init_pos = Vector2();
-var exhaust_turn_ofs = Vector2(3, 0);
+var left_exhaust_init_pos = Vector2()
+var right_exhaust_init_pos = Vector2()
+var ship_type: int = ShipType.SHIELD
 
 
 signal fire(position, velocity)
@@ -33,18 +66,12 @@ const FIRE_SPEED = 600.0
 const FIRE_COOLDOWN = 5
 
 
-enum Frames {
-    FLAT = 0,
-    LEFT = 1,
-    RIGHT = 2,
-}
-
-
 func make_snapshot():
     return {
         "health": health,
         "position": position,
         "fire_cooldown": fire_cooldown,
+        "ship_type": ship_type,
     }
 
 
@@ -54,11 +81,13 @@ func restore_snapshot(snapshot):
             "health": 0,
             "position": Vector2(0, 0),
             "fire_cooldown": 0,
+            "ship_type": ShipType.BULLET,
         }
 
     set_health(snapshot["health"])
     position = snapshot["position"]
     fire_cooldown = snapshot["fire_cooldown"]
+    ship_type = snapshot["ship_type"]
 
     if health <= 0:
         queue_free()
@@ -67,7 +96,7 @@ func restore_snapshot(snapshot):
 func damage_effect() -> void:
     var s= Color(1,1,1,1)
     var e= Color(6,6,6,6)
-    tween.interpolate_property(image, "modulate:a",
+    tween.interpolate_property(image, "modulate",
             s, e, 0.1)
     tween.start()
     yield(tween, "tween_completed")
@@ -77,8 +106,8 @@ func damage_effect() -> void:
 
 
 func _ready():
-    left_exhaust_init_pos = left_exhuast.position;
-    right_exhaust_init_pos = right_exhuast.position;
+    left_exhaust_init_pos = left_exhuast.position
+    right_exhaust_init_pos = right_exhuast.position
     right_exhuast.set_mod(10)
     var screen_size = get_viewport_rect().size
 
@@ -86,6 +115,42 @@ func _ready():
     position.y = screen_size.y - 100
 
     $Area2D.connect("area_shape_entered", self, "_on_area_shape_entered")
+
+
+func _update_ship(velocity: Vector2) -> void:
+    """update the visual of the ship"""
+    var conf = CONFIG[ship_type]
+    var exhaust_turn_ofs = conf['exhaust_turn_ofs']
+    if velocity.x == 0:
+        image.set_frame(conf['frames'][Frames.FLAT])
+        left_exhuast.position = left_exhaust_init_pos
+        right_exhuast.position = right_exhaust_init_pos
+        left_exhuast.set_trail_bend(0)
+        right_exhuast.set_trail_bend(0)
+    elif velocity.x < 0:
+        image.set_frame(conf['frames'][Frames.LEFT])
+        left_exhuast.position = left_exhaust_init_pos - exhaust_turn_ofs + Vector2(0, conf['exhaust_turn_y_ofs'])
+        right_exhuast.position = right_exhaust_init_pos - exhaust_turn_ofs
+        left_exhuast.set_trail_bend(-200)
+        right_exhuast.set_trail_bend(-200)
+    else:
+        image.set_frame(conf['frames'][Frames.RIGHT])
+        left_exhuast.position = left_exhaust_init_pos + exhaust_turn_ofs
+        right_exhuast.position = right_exhaust_init_pos + exhaust_turn_ofs + Vector2(0, conf['exhaust_turn_y_ofs'])
+        left_exhuast.set_trail_bend(200)
+        right_exhuast.set_trail_bend(200)
+
+    if velocity.y == 0:
+        left_exhuast.set_thrust(1.1)
+        right_exhuast.set_thrust(1.1)
+    elif velocity.y > 0:
+        left_exhuast.set_thrust(0.5)
+        right_exhuast.set_thrust(0.5)
+    elif velocity.y < 0:
+        left_exhuast.set_thrust(1.8)
+        right_exhuast.set_thrust(1.8)
+
+
 
 
 func run_step(inputs: InputState, delta: float) -> void:
@@ -100,34 +165,7 @@ func run_step(inputs: InputState, delta: float) -> void:
     if inputs.is_pressed(InputType.RIGHT):
         velocity.x += HORIZONTAL_SPEED
 
-    if velocity.x == 0:
-        image.set_frame(Frames.FLAT)
-        left_exhuast.position = left_exhaust_init_pos
-        right_exhuast.position = right_exhaust_init_pos
-        left_exhuast.set_trail_bend(0)
-        right_exhuast.set_trail_bend(0)
-    elif velocity.x < 0:
-        image.set_frame(Frames.LEFT)
-        left_exhuast.position = left_exhaust_init_pos - exhaust_turn_ofs
-        right_exhuast.position = right_exhaust_init_pos - exhaust_turn_ofs
-        left_exhuast.set_trail_bend(-200)
-        right_exhuast.set_trail_bend(-200)
-    else:
-        image.set_frame(Frames.RIGHT)
-        left_exhuast.position = left_exhaust_init_pos + exhaust_turn_ofs
-        right_exhuast.position = right_exhaust_init_pos + exhaust_turn_ofs
-        left_exhuast.set_trail_bend(200)
-        right_exhuast.set_trail_bend(200)
-
-    if velocity.y == 0:
-        left_exhuast.set_thrust(1.1)
-        right_exhuast.set_thrust(1.1)
-    elif velocity.y > 0:
-        left_exhuast.set_thrust(0.5)
-        right_exhuast.set_thrust(0.5)
-    elif velocity.y < 0:
-        left_exhuast.set_thrust(1.8)
-        right_exhuast.set_thrust(1.8)
+    _update_ship(velocity)
 
     var screen_size = get_viewport_rect().size
 
@@ -145,6 +183,10 @@ func run_step(inputs: InputState, delta: float) -> void:
     if health <= 0:
         emit_signal("death", position)
         queue_free()
+
+
+func set_type(new_type: int) -> void:
+    ship_type = new_type
 
 
 func set_health(new_health: float) -> void:
