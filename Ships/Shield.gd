3537db70a1e1347760486a270b14153f06430d2b
Robert McGregor <rmcgregor1990@gmail.com>
2021-06-13 16:51:26 +0100

    sheild flash

--- Ships/Shield.gd
+++ Ships/Shield.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 
 onready var bullet_manager: BulletManager = $"/root/Main/BulletManager"
+onready var tween: Tween = $Tween
+onready var image = $Sprite
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,8 +16,22 @@ func _on_area_shape_entered(_area_id: int, _area: Area2D, area_shape: int, _loca
         var bullet = bullet_manager.get_bullet(area_shape)
         if !bullet.is_player:
             bullet.dead = true
+            damage_effect()
 
 
 func set_state(on: bool) -> void:
     if visible != on:
         visible = on
+
+
+func damage_effect() -> void:
+    if not tween.is_active():
+        var s= Color(1,1,1,1)
+        var e= Color(2,2,2,2)
+        tween.interpolate_property(image, "modulate",
+                s, e, 0.1)
+        tween.start()
+        yield(tween, "tween_completed")
+        tween.interpolate_property(image, "modulate",
+                e, s, 0.1)
+        tween.start()
