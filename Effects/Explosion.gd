9dd962a9affa27b0b5d45bc5b6407978abaf152a
Robert McGregor <rmcgregor1990@gmail.com>
2021-06-13 18:17:44 +0100

    cleanup explosion

--- Effects/Explosion.gd
+++ Effects/Explosion.gd
@@ -1,10 +1,6 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 onready var p1: Particles2D = $Particles;
 onready var p2: Particles2D = $Particles2;
 
@@ -19,7 +15,7 @@ func _ready():
 func run():
     p1.emitting = true
     p2.emitting = true
-    yield(get_tree().create_timer(3), "timeout")
+    yield(get_tree().create_timer(4.0), "timeout")
     queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
